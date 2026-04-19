ddb2ff47058c31164a749b655e8b1b90a5368095
jomrembau <jomir.bautista@gmail.com>
2026-04-19 12:29:27 +0200

    Create CombineStrings.java

---
+++ CombineStrings.java
@@ -0,0 +1,9 @@
+public class CombineStrings {
+    public static void main(String[] args) {
+        System.out.println(combineNames("dan", "dadan"));
+    }
+     public static String combineNames(String first,String last) {
+        String combo = first +" "+ last;
+        return combo;
+    }
+}
